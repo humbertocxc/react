b1e492ae321d2c29f65cae1a50a62e81cb710523
humbertocxc <humberto.gnm98@gmail.com>
2021-09-16 03:08:46 -0300

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import './styles/global.css';
+
 export function App() {
-    throw new Error('Erro Erro Erro Erro');
-    
+    // throw new Error('Erro Erro Erro Erro');
+
     return(
         <h1>Hello World</h1>
     )
