9b23494f40d7d3e2a3810448b786b158bc72f988
humbertocxc <humberto.gnm98@gmail.com>
2021-09-16 16:06:48 -0300

    listagem dos itens

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,9 +1,13 @@
 export function RepositoryItem(props) {
     return (
         <li>
-        <strong>{props.repository?.name ?? 'Default'}</strong>
-        <p>{props.repository?.description ?? 'Default'}</p>
-        <a href={props.repository?.link ?? '#'}>Acessar repositórios</a>
+        <strong>{props.repository?.name}</strong>
+
+        <p>{props.repository?.description}</p>
+
+        <a href={props.repository?.html_url}>
+            Acessar repositórios
+        </a>
     </li>
     );
 }
