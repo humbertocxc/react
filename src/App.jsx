5a9d53bc9523282a6beb10f0dea374aa7cf05fab
humbertocxc <humberto.gnm98@gmail.com>
2021-09-16 17:10:26 -0300

    typescript

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import './styles/global.scss';
 import { RepositoryList } from './components/RepositoryList';
 
 export function App() {
-  // throw new Error('Erro Erro Erro Erro');
-
   return(
     <>
       <RepositoryList />
