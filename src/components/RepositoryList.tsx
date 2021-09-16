db8193e7890a2444d04c7b14abdf663b55da91c8
humbertocxc <humberto.gnm98@gmail.com>
2021-09-16 17:26:13 -0300

    typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,17 +3,15 @@ import { useState, useEffect } from "react";
 
 import '../styles/repositories.scss'
 
-// https://api.github.com/orgs/rocketseat/repos
 
-
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
 }
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
