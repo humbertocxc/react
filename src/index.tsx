65a27bd5f09f33d7a39cbf13169a6d7067597fa0
humbertocxc <humberto.gnm98@gmail.com>
2021-09-17 16:06:30 -0300

    listando transacoes

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,30 @@ import { App } from './App';
 
 createServer( {
   models: {
-    transaction: Model,
+    transactions: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id:1,
+          title: 'Freelance de Website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id:2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-05 14:00:00'),
+        }
+      ],
+    })
   },
 
   routes() {
@@ -17,7 +40,7 @@ createServer( {
 
     this.post('/transactions', (schema, request) => {
       const data = JSON.parse(request.requestBody)
-      return schema.create('transaction', data)
+      return schema.create('transactions', data)
     })
   }
 })
