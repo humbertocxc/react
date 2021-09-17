2d29be707c074e0a514dd694b6da495dd73f159f
humbertocxc <humberto.gnm98@gmail.com>
2021-09-17 01:07:02 -0300

    fonts

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -30,6 +30,20 @@ export const GlobalStyle = createGlobalStyle`
     }
   }
 
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
   button {
     cursor: pointer;
   }
@@ -38,11 +52,4 @@ export const GlobalStyle = createGlobalStyle`
     opacity: 0.6;
     cursor: not-allowed;
   }
-
-  body {
-    background: var(--background);
-    -webkit-font-smoothing: antialiased;
-  }
-
-
 `
