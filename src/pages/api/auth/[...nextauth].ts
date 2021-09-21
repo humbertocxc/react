da9ec23b32f2866bebc5df4a82fdad5553bfd99a
humbertocxc <humberto.gnm98@gmail.com>
2021-09-21 19:15:36 -0300

    verificando usuario repetido

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -13,19 +13,38 @@ export default NextAuth({
     }),
   ],
   callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
+    async signIn({ user }) {
+
+      const { email } = user
+
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            { data: { email: { verificationRequest: true } } },
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email),
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } },
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email),
+              )
+            )
           )
         )
+
+        return true
       } catch {
         return false
       }
-
-      return true
     },
   }
 })
