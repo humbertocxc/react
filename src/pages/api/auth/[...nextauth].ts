311638cfd37e787cb132fe20719592394cee20f9
humbertocxc <humberto.gnm98@gmail.com>
2021-09-21 18:21:44 -0300

    adicionando usuario ao db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -8,4 +12,20 @@ export default NextAuth({
       clientSecret: process.env.CLIENT_SECRET_ID,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email: { verificationRequest: true } } },
+          )
+        )
+      } catch {
+        return false
+      }
+
+      return true
+    },
+  }
 })
